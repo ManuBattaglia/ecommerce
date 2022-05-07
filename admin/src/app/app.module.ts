48c4c9a6f642601736dbb9d5660c2e501d26128e
ManuBattaglia <manubattaglia94@gmail.com>
2022-05-07 17:01:45 -0300

    agrego carpeta admin

---
+++ admin/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+import { InicioComponent } from './components/inicio/inicio.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    HttpClientModule,
+    routing,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
