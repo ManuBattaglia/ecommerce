34050a55bfd82d2bfff187343c526addfbf259cd
ManuBattaglia <manubattaglia94@gmail.com>
2022-05-11 20:46:43 -0300

    Login Admin Terminado

--- admin/src/app/components/login/login.component.ts
+++ admin/src/app/components/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from 'src/app/services/admin.service';
 
 declare var jQuery: any;
 declare var $:any;
+declare var iziToast: any;
 
 @Component({
   selector: 'app-login',
@@ -10,11 +12,44 @@ declare var $:any;
 })
 export class LoginComponent implements OnInit {
 
-  constructor() {
+  public user: any={};
+
+  constructor(
+    private _adminService: AdminService
+  ) {
    
    }
 
   ngOnInit(): void {
   }
 
+  // RECIBE POR PARAMETRO TODO EL FORM
+  login(loginForm:any){
+    if(loginForm.valid){
+      console.log(this.user);
+
+      let data = {
+        mail: this.user.mail,
+        password: this.user.password
+      }
+
+      this._adminService.login_admin(data).subscribe(
+        response=>{
+          console.log(response);
+        },
+        error=>{
+          console.log(response);
+        }
+      );
+    }
+    else{
+      iziToast.show({
+          title: 'ERROR',
+          titleColor: '#FF0000',
+          class:'text-danger',
+          position: 'topRight',
+          message: 'Los datos del formulario no son válidos'
+      });
+    }
+  }
 }
--- back/app.js
+++ back/app.js
@@ -20,43 +20,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/tienda', (err, res) =>{
     }
 })
 
-// var server = require('http').createServer(app);
-// var io = require('socket.io')(server,{
-//     cors: {origin : '*'}
-// });
-
-
-// io.on('connection',function(socket){
-//     socket.on('delete-carrito',function(data){
-//         io.emit('new-carrito',data);
-//         console.log(data);
-//     });
-
-
-//     socket.on('add-carrito-add',function(data){
-//         io.emit('new-carrito-add',data);
-//         console.log(data);
-//     });
-    
-// });
-
-
- 
-// var admin_routes = require('./routes/admin');
-// var cupon_routes = require('./routes/cupon');
-
-// mongoose.connect('mongodb://127.0.0.1:27017/tienda',{useUnifiedTopology: true, useNewUrlParser: true}, (err,res)=>{
-//     if(err){  
-//         throw err;
-//         console.log(err);
-//     }else{
-//         console.log("Corriendo....");
-//         server.listen(port, function(){
-//             console.log("Servidor " + port );
-//         });
-
-//     }
-// });
 
 //ANALIZAR CUERPO PETICIONES
 app.use(bodyparser.urlencoded({limit: '50mb',extended:true}));
